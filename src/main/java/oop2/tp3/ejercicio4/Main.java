a21a7da59a4737b6db392e45f5a28e3c985db16f
Agustin <114381140+PueblaTL@users.noreply.github.com>
2025-04-29 18:32:21 -0300

    ejercicio4

--- src/main/java/oop2/tp3/ejercicio4/Main.java
+++ src/main/java/oop2/tp3/ejercicio4/Main.java
@@ -2,6 +2,7 @@ package oop2.tp3.ejercicio4;
 
 import org.jdbi.v3.core.Jdbi;
 
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,17 +12,17 @@ public class Main {
         new SetUpDatabase(jdbi).setUp();
 
         var repo = new PersonaRepository(jdbi);
-        var personas = repo.buscarPorNombre("Vla");
-
-        if (personas != null) {
+        try {
+            var personas = repo.buscarPorNombre("Vla");
             for (Persona persona : personas) {
                 System.out.println(persona.nombre() + " " + persona.apellido());
             }
+
+            var persona = repo.buscarId(40L);
+            System.out.println(persona.nombre() + " " + persona.apellido());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
         }
 
-        var persona = repo.buscarId(1L);
-        if (persona != null) {
-            System.out.println(persona.nombre() + " " + persona.apellido());
-        }
     }
 }
